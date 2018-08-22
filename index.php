5722666042cd3a555f6b3c0cf38a9d34a06a5806
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-08-22 11:50:17 +0200

    correction

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Exercice 5</title>
-  </head>
-  <body>
-    <form class="form" action="index.php" method="POST">
-      <select class="select" name="civility">
-        <option>Mr</option>
-        <option>Mme</option>
-      </select>
-      <input type="text" name="lastname" placeholder="Nom" />
-      <input type="text" name="firstname" placeholder="Prénom" />
-      <input type="submit" name="valider" value="Valider">
-    </form>
-    <?php
-if (isset($_POST['lastname']) && isset($_POST['firstname']) && isset($_POST['civility']))
-{
-  echo 'Bonjour ' . $_POST['civility'] . ' ' . $_POST['lastname'] . ' ' . $_POST['firstname'];
-} else {
-  echo 'Veuillez renseigner votre identité svp!';
-}
-
-     ?>
-  </body>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice 5</title>
+    </head>
+    <body>
+        <form class="form" action="index.php" method="POST">
+            <label>Informations :</label><select class="select" name="civility">
+                <option selected disabled>Veuillez selectionner une option</option>
+                <option>Mr</option>
+                <option>Mme</option>
+            </select>
+            <label>Nom :</label><input type="text" name="lastname" placeholder="Nom" />
+            <label>Prénoms :</label><input type="text" name="firstname" placeholder="Prénom" />
+            <input type="submit" name="valider" value="Valider">
+        </form>
+        <?php
+        if (isset($_POST['lastname']) && isset($_POST['firstname']) && isset($_POST['civility'])) {
+            echo 'Bonjour ' . $_POST['civility'] . ' ' . $_POST['lastname'] . ' ' . $_POST['firstname'];
+        } else {
+            echo 'Veuillez renseigner votre identité svp!';
+        }
+        ?>
+    </body>
 </html>
